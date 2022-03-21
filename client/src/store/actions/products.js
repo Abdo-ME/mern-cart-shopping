19bc3f71ec2c0676884eae823daf596f2be01658
Abdo-ME <abdelhakmehda@gmail.com>
2022-03-21 15:24:10 +0100

    handle filter by redux

--- client/src/store/actions/products.js
+++ client/src/store/actions/products.js
@@ -1,8 +1,10 @@
-import { FETCH_PRODUCTS } from "./types"
+import { FETCH_PRODUCTS, FILTER_ORDER, FILTER_SIZE } from "./types"
 
 
 export const fetchProducts = () => {
     return dispatch => {
+
+
         fetch("/api/products")
             .then(res => res.json())
             .then(data => {
@@ -12,4 +14,46 @@ export const fetchProducts = () => {
                 })
             })
     }
+}
+
+//Filter By Order
+export const filteredSort = (products, value) => {
+        
+    return dispatch => {
+    const cloneProducts = [...products];
+    let newProducts = cloneProducts.sort((a, b) => {
+        if (value === "lowest") {
+        return  a.price - b.price
+        } else if (value === "highest") {
+        return b.price - a.price    
+        }  else{
+        return a.id < b.id ? 1 : -1
+        }
+    });
+    dispatch({
+        type: FILTER_ORDER,
+        data: {
+            sort: value,
+            products: newProducts
+        }
+    })
+    }
+
+}
+//Filter By Size
+
+export const filteredSize = (products, value,allproducts) => {
+    // console.log(products);
+    
+    return dispatch => {
+        let cloneProducts = [...products];
+        let newProducts = cloneProducts.filter(p => p.sizes.includes(value))
+        dispatch({
+            type: FILTER_SIZE,
+            data: {
+                size: value,
+                products: value === "ALL"? allproducts:newProducts
+            }
+        })
+    }   
 }
--- client/src/store/reducers/productsReducer.js
+++ client/src/store/reducers/productsReducer.js
@@ -1,12 +1,64 @@
-import { FETCH_PRODUCTS } from "../actions/types";
+import { FETCH_PRODUCTS, FILTER_SIZE, FILTER_ORDER } from "../actions/types";
 
-
-export const productsReducer = (state = {}, action) => {
-    switch (action.type) {
+export const productsReducer = (state= {}, action) => {
+    switch(action.type) {
         case FETCH_PRODUCTS:
-            return {products:action.data}
-    
+            return {products: action.data, filterProducts: action.data} 
+        
+        case FILTER_SIZE: 
+            return {
+                ...state,
+                size: action.data.size,
+                filterProducts: action.data.products
+            }   
+            
+        case FILTER_ORDER: 
+        return {
+            ...state,
+            sort: action.data.sort,
+            filterProducts: action.data.products
+        }   
         default:
-            return state;
+            return state
     }
 }
+
+
+
+
+
+
+
+// import { FETCH_PRODUCTS,FILTER_ORDER, FILTER_SIZE} from "../actions/types";
+
+
+// export const productsReducer = (state = {}, action) => {
+    
+//     switch (action.type) {
+      
+//     case FETCH_PRODUCTS:
+//         // console.log(action.data);
+//             return {
+//                 products: action.data,
+//                 filterProducts: action.data.products,
+//                 sort: action.data.sort
+//             }
+        
+//         case FILTER_SIZE:
+//             console.log("actived");
+//         return {
+//             ...state,
+//             filterProducts: action.data.products,
+//             size : action.data.size
+//         }
+        
+//         case FILTER_ORDER:
+//         return {
+//             ...state
+//             , filterProducts: action.data.products,
+//             sort : action.data.sort
+//         }
+//     default:
+//             return state;
+//     }
+// }
