300ecab680614681146f98aff8b2125d8b868c63
Abdo-ME <abdelhakmehda@gmail.com>
2022-03-23 11:52:32 +0100

    Create Order Component

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,13 +1,17 @@
 import "../../css/Cart/cart.css"
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Modal from "react-modal"
 import Checkout from "../CheckoutForm/Checkout"
 import { connect } from "react-redux";
 import { removeProductCart } from "../../store/actions/cart";
 
 
+
 function Cart({ productsCart, handleRemoveProduct,removeProductCart }) {
     const [showForm, setShowForm] = useState(false);
     const [value, setValue] = useState("");
+    const [order, setOrder] = useState("");
+    const [isOpen,setIsOpen]=useState(false)
     const handleChange = (e) => {
         setValue((prevValue) => (
             { ...prevValue, [e.target.name]: e.target.value })
@@ -19,7 +23,7 @@ function Cart({ productsCart, handleRemoveProduct,removeProductCart }) {
             name: value.name,
             email: value.email
         }
-        
+        setOrder(order);
 
     }
 return (
@@ -27,6 +31,39 @@ return (
         <div className="cart-title">
             <p>{productsCart.length? `you have ${productsCart.length} product${productsCart.length ===1?"":"s"} `: "Your Cart is Empty" } </p>
         </div>
+            {/* Model */}
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} ariaHideApp={false} >
+        <span className='icon-times' onClick={()=>setIsOpen(false)}>&times; </span>
+            
+            <div className="order-info">
+                <p className="alert-success"> Order Done  Success</p>
+                <table>
+                    <tr>
+                        <td>Name:</td>
+                        <td>{ order.name}</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td>${ order.email}</td>
+                    </tr>
+                    <tr>
+                        <td>Total:</td>
+                        <td>{ productsCart.reduce((a,p)=>p.price+a,0)}</td>
+                    </tr>
+                    <tr>
+                        <td>Selected Products:</td>
+                        <td>{productsCart.map((p) => {
+                                return 
+                                (<>
+                                <p>Product Name: {p.title} </p>
+                                <p>Quentity:{p.qty} </p>
+                            </>)
+                        })}</td>
+                    </tr>
+                </table>
+            </div>
+        </Modal>
+
         {productsCart.map(product => (
             <div key={product.id} className="cart-info">
             <img src={product.imageUrl} alt={product.title} />
@@ -49,7 +86,7 @@ return (
         </div>
         )}
         {/* {checkout form} */}
-    <Checkout showForm={showForm} setShowForm={setShowForm} handlecheckout={handlecheckout} handleChange={handleChange} />
+    <Checkout setOrder={setOrder} setIsOpen= {setIsOpen} showForm={showForm} setShowForm={setShowForm} handlecheckout={handlecheckout} handleChange={handleChange} />
 </div>
 )
 }
